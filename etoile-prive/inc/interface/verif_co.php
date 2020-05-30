06afc5ca5fc8fed9db8fc379b1e57537cfbe3f77
guillaumepoucet <poucet@simplon-charleville.fr>
2020-05-30 11:55:12 +0200

    verif support admin ok

--- etoile-prive/inc/interface/verif_co.php
+++ etoile-prive/inc/interface/verif_co.php
@@ -1,6 +1,9 @@
 <?php
-    if(!isset($_SESSION['user'])) {
-        header('location:loggin.php');
+if (!isset($_SESSION['user'])) {
+    header('location:loggin.php');
+}
+if (strpos($_SERVER['REQUEST_URI'], '/support.php') !== false) {
+    if ($_SESSION['type'] != 2) {
+        header('location:index.php');
     }
-
-?>
+}
